a3f145fb640b0e3a2a08c0c30c2a917433065941
Dustin Walker <dwtimp@gmail.com>
2016-10-18 10:08:12 -0500

    added divs to index.html

---
+++ app/spotify-tracker/index.js
@@ -0,0 +1,16 @@
+import 'whatwg-fetch';
+// import data from the json file into index.js
+import  data from './data.json';
+import Model from './track-model';
+import SpotifyTrack from  './track-view';
+
+// loop
+const results = document.querySelector('.results');
+
+data.tracks.items.forEach((item) => {
+  const t = new Model(item);
+
+  const view = new SpotifyTrack(t);
+
+  document.body.appendChild(view);
+});
--- app/spotify-tracker/track-view.js
+++ app/spotify-tracker/track-view.js
@@ -2,25 +2,22 @@
 // create a new model module for spotify track that exports a class
 
 export default class SpotifyTrack {
-  constructor (data){
-
-    this.data = document.createElement('div');
-    this.data.classList.add('track');
-    this.data.classList.add('panel-block');
-    this.data.innerHTML = `
+  constructor(data) {
+    const container = document.createElement('div');
+    container.classList.add('track');
+    container.classList.add('panel-block');
+    container.innerHTML = `
       <p class="track__artist"></p>
       <p class="track__name"></p>
       <a class="track__url"></a>
       <img class="track__image"></img>`;
-    }
+
+    this.render(data);
   }
-  render() {
+  render()  {
     this.data.querySelector('.track__artist').innerText = this.data.artist;
     this.data.querySelector('.track__name').innerText = this.data.name;
     this.data.querySelector('.track__url').innerText = this.data.url;
     this.data.querySelector('.track__image').innerText = this.data.url;
-
-  };
-
-
-}
+  }
+  }
