29022abe33b723ab1e8fd06cdd4cf0e0480ff043
Dustin Walker <dwtimp@gmail.com>
2016-11-21 22:09:14 -0600

    working on track-view

--- app/spotify-tracker/track-view.js
+++ app/spotify-tracker/track-view.js
@@ -8,17 +8,31 @@ export default class SpotifyTrack {
     this.el.classList.add('track');
     this.el.classList.add('panel-block');
     this.el.innerHTML = `
-      <p class="track__artist"></p>
-      <p class="track__name"></p>
-      <img class="track__img">
-      <a class="track__link"></a></img>`;
+    <div class="content">
+      <div class="content-left">
+        <div class="track__frame">
+          <img class="track__img src=" alt="">
+          </div>
+        </div>
+      <div class="content-center">
+        <h2 class="track__artist">
+          <h4 class="track__name">
+      </div>
+      <div class="content-right">
+        <div class="track__url">
+          <a class="track__link">
+            <span class="fa fa-spotify fa-3x"</span>
+          </a>
+        </div>
+      </div>
+    </div>`;
 
     this.render();
   }
   render()  {
     this.data.querySelector('.track__artist').innerText = this.data.artist;
     this.data.querySelector('.track__name').innerText = this.data.name;
-    this.data.querySelector('.track__img').innerText = this.data.url;
+    this.data.querySelector('.track__img').innerText = this.data.image;
     this.data.querySelector('.track__link').innerText = this.data.url;
   }
   }
