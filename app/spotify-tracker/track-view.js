14602f531668acb873d1fd1cc8fa7d258b3409e8
Dustin Walker <dwtimp@gmail.com>
2016-11-21 20:04:28 -0600

    edited track view.js

--- app/spotify-tracker/track-view.js
+++ app/spotify-tracker/track-view.js
@@ -4,16 +4,16 @@
 export default class SpotifyTrack {
   constructor(data) {
     this.data = data;
-    const container = document.createElement('div');
-    container.classList.add('track');
-    container.classList.add('panel-block');
-    container.innerHTML = `
+    this.el = document.createElement('div');
+    this.el.classList.add('track');
+    this.el.classList.add('panel-block');
+    this.el.innerHTML = `
       <p class="track__artist"></p>
       <p class="track__name"></p>
       <img class="track__img">
       <a class="track__link"></a></img>`;
 
-    this.render(data);
+    this.render();
   }
   render()  {
     this.data.querySelector('.track__artist').innerText = this.data.artist;
--- public/index.js
+++ public/index.js
@@ -4,7 +4,6 @@ import data from './data.json';
 import Model from './track-model';
 import SpotifyTrack from './track-view';
 
-// loop
 
 data.track.items.forEach((item) => {
   const t = new Model(item);
